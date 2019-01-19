ae4a4154fa15752e18c7db7cbca537843dc62f17
Ethan <ethanbealbrown@gmail.com>
2019-01-19 15:41:33 -0500

    change ip for db

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post('/postSymposiumSpanish', function(req, res) {
 var writetoDB = function(req, res, whichDB) {
 
     var connection = mysql.createConnection({
-        host: 'localhost',
+        host: '198.199.91.241',
         user: 'ethan',
         password: process.env.ABSPASS
     });
