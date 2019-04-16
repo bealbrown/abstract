0ab4f07548527c635e1140d91c1f31ac0f5e31cc
fsdfasdf <asdf asdf>
2019-04-15 17:01:32 -0700

    adding export function

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+// const { Parser } = require('json2csv');
+var Excel = require('exceljs');
 
 const app = express()
 const port = 3000
@@ -38,6 +40,26 @@ app.get('/symposium', function(req, res) {
 
 app.get('/symposium_es', function(req, res) {
     res.render('symposium_es.pug');
+})
+
+app.get('/' + process.env.EXPORTSTR, function(req, res) {
+    res.render('export.pug', { exportstr: process.env.EXPORTSTR} );
+})
+
+app.get('/' + process.env.EXPORTSTR + 'abstractExport', function(req, res) {
+    exportDB(req,res,'mainEnglish');
+})
+
+app.get('/' + process.env.EXPORTSTR + 'abstractSpanishExport', function(req, res) {
+    exportDB(req,res,'mainSpanish');
+})
+
+app.get('/' + process.env.EXPORTSTR + 'symposiumExport', function(req, res) {
+    exportDB(req,res,'Symposium');
+})
+
+app.get('/' + process.env.EXPORTSTR + 'symposiumSpanishExport', function(req, res) {
+    exportDB(req,res,'SymposiumSpanish');
 })
 
 
@@ -112,4 +134,137 @@ var writetoDB = function(req, res, whichDB) {
     return;
 }
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+var exportDB = function(req,res,whichDB) {
+
+    var connection = mysql.createConnection({
+        host: '198.199.91.241',
+        user: 'ethan',
+        password: process.env.ABSPASS
+    });
+
+    connection.connect(function(err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+    });
+
+    connection.query('SELECT * FROM abstract.   ' + whichDB, function(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+
+        } else {
+
+            try {
+
+                var workbook = new Excel.Workbook();
+                var mainSheet = workbook.addWorksheet('Main');
+                var authorSheet = workbook.addWorksheet('Authors');
+                var topicSheet = workbook.addWorksheet('Topics');
+                var rowsObj = JSON.parse(JSON.stringify(result));
+
+                colObj = rowsObj[0];
+                delete colObj.authors;
+                delete colObj.topics;
+                columnsData = Object.keys(colObj);                
+                mainSheet.addRow([whichDB]);     
+                mainSheet.addRow([""]);     
+                mainSheet.addRow(columnsData);     
+
+                // Main Sheet
+
+                for (i = 0; i < rowsObj.length; i++ ) {
+
+                    authorsJSON = rowsObj[i]["authors"];   
+                    topicsJSON = rowsObj[i]["topics"]; 
+
+                    delete rowsObj[i].authors;
+                    delete rowsObj[i].topics;
+
+                    rowData = Object.values(rowsObj[i]);                
+                    mainSheet.addRow(rowData);
+
+                    function IsJsonString(str) {
+                        try {
+                            JSON.parse(str);
+                        } catch (e) {
+                            return false;
+                        }
+                        return true;
+                    }
+
+                    var idRow = authorSheet.addRow([rowsObj[i]["id"],rowsObj[i]["englishTitleInput"]]);
+                    idRow.fill = {type: 'pattern', pattern: 'darkVertical', fgColor: {argb: 'CACACA'}};
+
+                    var idRow = topicSheet.addRow([rowsObj[i]["id"],rowsObj[i]["englishTitleInput"]]);
+                    idRow.fill = {type: 'pattern', pattern: 'darkVertical', fgColor: {argb: 'CACACA'}};
+
+
+                    if (IsJsonString(authorsJSON)) {
+
+                        authors = JSON.parse(authorsJSON);
+
+                       
+                        authorCols = Object.keys(authors[0]);   
+                        authorCols.unshift("");             
+                        authorSheet.addRow(authorCols);    
+
+                        authors.forEach(function(author){
+
+                            rowData = Object.values(author);    
+                            rowData.unshift("");
+                            authorSheet.addRow(rowData);
+
+                        });
+
+                        authorSheet.addRow(null);
+                    } 
+
+                    if (IsJsonString(topicsJSON)) {
+
+                        topics = JSON.parse(topicsJSON);
+
+                        topicCols = Object.keys(topics[0]);   
+                        topicCols.unshift("");             
+                        topicSheet.addRow(topicCols);    
+
+                        topics.forEach(function(topic){
+
+                            rowData = Object.values(topic);    
+                            rowData.unshift("");
+                            topicSheet.addRow(rowData);
+
+                        });
+
+                        topicSheet.addRow(null);
+                    } 
+                }   
+
+            var fileName = whichDB + '.xlsx';
+
+            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            res.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+
+            workbook.xlsx.write(res).then(function(){
+                res.end();
+            });          
+
+
+            } catch (err) {
+              console.error(err);
+            }
+        }
+    });
+
+    connection.end(function(err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+}
+
+app.listen(port, () => 
+    console.log(`Listening on port ${port}!`)
+)
